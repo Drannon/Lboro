b900b62e2108e52e9a1880c1fc7c6f0f34145e05
Matthew Morton <matthew.morton34@yahoo.co.uk>
2026-06-05 13:48:07 +0100

    added wetted area and vofd helpers

--- MBSE/SysDes/SCRIPTS/util.py
+++ MBSE/SysDes/SCRIPTS/util.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from numpy import typing as npt
+from scipy import simpson
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
@@ -25,3 +26,17 @@ def rstool(X: npt.ArrayLike, y: npt.ArrayLike, X_test: npt.ArrayLike) -> np.ndar
 def ms2kt(V_ms: float) -> float:
     """Convert a speed in metres per second to knots."""
     return (900 * V_ms) / 463
+
+
+def volumeOfDisplacement(
+    T: float, B: float, LPP: float, hullProf: np.typing.arrayLike
+) -> float:
+    """Calculate the volume of fluid displaced by the vessel, given a profile, beam, and draught."""
+    xSecA = (B * T) - simpson(hullProf)
+    return LPP * xSecA
+
+
+def wettedArea(v_of_D: float, T: float, LPP: float, c_R: float, b_R: float) -> float:
+    """Find the wetted area of a vessel, including rudder."""
+    S = (1.025 * ((v_of_D / T) + (1.7 * LPP * T))) + (c_R * b_R)
+    return S
