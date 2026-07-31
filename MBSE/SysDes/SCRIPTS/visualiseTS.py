7a54bbb7c20014cad8708a723847394d8056f1ca
Matthew Morton <matthew.morton34@yahoo.co.uk>
2026-07-31 15:05:02 +0100

    optimising one variable (TC). Seems very low but features look ok

--- MBSE/SysDes/SCRIPTS/visualiseTS.py
+++ MBSE/SysDes/SCRIPTS/visualiseTS.py
@@ -10,17 +10,10 @@ import matplotlib.pyplot as plt
 
 matplotlib.use("gtk4agg")
 
-# DESIGN CONSTANTS
-draught = 1.2  # m
-beam = 25  # m
-turn_speed = 0.7 * util.kt2ms(8)  # m/s, % of cruising speed
-price_steel = 25  # £/kg, approximate for Grade S355G5
-deck_thickness = 10  # mm
-
 # DESIGN VARIABLES
 LPP_range = [60, 80]  # m
-rudder_chord_range = [1.5 * draught, 3 * draught]  # m
-rudder_span_range = [0.8 * draught, draught]  # m
+rudder_chord_range = [1.5 * util.draught, 3 * util.draught]  # m
+rudder_span_range = [0.8 * util.draught, util.draught]  # m
 rudder_deflection_range = [20, 35]  # degrees
 thruster_power_range = [5, 500]  # kW
 hull_control_point_range = [0, 1]  # % of draught
@@ -61,36 +54,26 @@ hull_thickness_samples = hull_thickness_range[0] + lhs_samples[:, 6] * (
     hull_thickness_range[1] - hull_thickness_range[0]
 )
 
+design_variables = [
+    LPP_samples,
+    rudder_span_samples,
+    rudder_chord_samples,
+    rudder_deflection_samples,
+    thruster_power_samples,
+    hull_control_point_samples,
+    hull_thickness_samples,
+]
+
 # Acceptance
 v_T_min = util.kt2ms(2)  # ?
 
-# Calculate the hull profiles for the samples of hull control points
-sampled_hull_X = []
-sampled_hull_Y = []
-
-for i, a in enumerate(hull_control_point_samples):
-    hull_Xs, hull_Ys = util.hull_profile_gen(a, beam, draught, 1000)
-    sampled_hull_X.append(hull_Xs)
-    sampled_hull_Y.append(hull_Ys)
-
-# Calculate volume of displacement for all combinations of samples
-sample_v_of_Ds = util.volumeOfDisplacement(
-    T=draught, B=beam, LPP=LPP_samples, hullXs=sampled_hull_X, hullYs=sampled_hull_Y
-)
-
-# Calculate wetted areas for all combinations of samples
-sample_wetted_areas = util.wettedArea(
-    sample_v_of_Ds, draught, LPP_samples, rudder_chord_samples, rudder_span_samples
-)
-
 # Calculate the traverse speeds for the DV samples
 
-sample_about_turns = de.computeAboutTurn60Time(
-    S=sample_wetted_areas, C_D=0.03, LOA=LPP_samples, P_t=thruster_power_samples
-)
-sample_traverse_speeds = de.computeTraverseSpeed(
-    P_t=thruster_power_samples, rho_w=1000, S=sample_wetted_areas, C_D=0.03
-)
+sample_traverse_speeds = []
+
+for i in range(n_sample):
+    dv_set = [dv[i] for dv in design_variables]
+    sample_traverse_speeds.append(de.computeTraverseSpeed(dv_set))
 
 # Create dataframe
 design_space_dict = {
@@ -108,7 +91,7 @@ design_space = pd.DataFrame(data=design_space_dict)
 feats = design_space.iloc[:, :-1]
 obs = design_space.iloc[:, -1:]
 
-pass_fail = np.where(design_space["V_t"] > v_T_max, "b", "r")
+pass_fail = np.where(design_space["V_t"] > v_T_min, "b", "r")
 
 # Plotting
 pd.plotting.scatter_matrix(design_space, c=pass_fail, alpha=1)
